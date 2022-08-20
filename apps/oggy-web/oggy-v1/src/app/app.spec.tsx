ebb083a4aa119238a5041516e81f16eb4c73b444
umpixel <madhurgoyal19@gmail.com>
2022-08-20 22:54:53 +0530

    hello

---
+++ apps/oggy-web/oggy-v1/src/app/app.spec.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+
+import App from './app';
+
+describe('App', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText(/Welcome oggy-web-oggy-v1/gi)).toBeTruthy();
+  });
+});
---
+++ apps/oggy-web/oggy-v1/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+      <NxWelcome title="oggy-web-oggy-v1" />
+    </>
+  );
+}
+
+export default App;
